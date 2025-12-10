ce2c6bec5a8e2c92d44d39deec44b645e3c72266
Dev Pedro <pedrova69@gmail.com>
2025-12-10 19:55:30 -0300

    Add box tileset and obstacle tilemap for stage 01

--- main/game/stages/stage_01/stage_01.go
+++ main/game/stages/stage_01/stage_01.go
@@ -6,6 +6,13 @@ components {
   id: "controller"
   component: "/main/game/stages/stages_controller.script"
 }
+components {
+  id: "obstacles_01"
+  component: "/main/game/stages/stage_01/obstacles_01.tilemap"
+  position {
+    z: 5.0
+  }
+}
 embedded_components {
   id: "wall"
   type: "collisionobject"
@@ -42,5 +49,19 @@ embedded_components {
   id: "cfactory_enemy_04"
   type: "collectionfactory"
   data: "prototype: \"/main/game/enemies/enemy_04/enemy_04.collection\"\n"
+  ""
+}
+embedded_components {
+  id: "obstacles"
+  type: "collisionobject"
+  data: "collision_shape: \"/main/game/stages/stage_01/obstacles_01.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"wall\"\n"
+  "mask: \"player\"\n"
+  "mask: \"bullet\"\n"
+  "mask: \"enemy\"\n"
   ""
 }
