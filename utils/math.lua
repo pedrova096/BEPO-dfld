52608fd1976f75d7d4b138ac16aad45353d411a2
Dev Pedro <pedrova69@gmail.com>
2026-06-26 20:59:13 -0500

    Implement camera follow

--- utils/math.lua
+++ utils/math.lua
@@ -4,6 +4,14 @@ function M.lerp(a, b, t)
   return a + (b - a) * t
 end
 
+---@param value number
+---@param min number
+---@param max number
+---@return number
+function M.clamp(value, min, max)
+  return math.max(min, math.min(max, value))
+end
+
 ---Get the heading angle from a vector.
 ---@param vector vector3
 ---@return number
